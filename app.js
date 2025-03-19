8a0e7133b939fbe27e5efe85b2ee3d597f5a0c20
Piero Gomez <77982106+pgomezboza@users.noreply.github.com>
2025-03-18 19:02:51 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = [];
+
+function agregarNuevoAmigo() {
+  const nombreDelAmigo = obtenerNombreDelAmigo();
+
+  if (esNombreValido(nombreDelAmigo)) {
+    listaDeAmigos.push(nombreDelAmigo);
+    limpiarCampoDeEntrada();
+    actualizarListaVisual();
+  } else {
+    alert("Por favor, inserte un nombre válido");
+  }
+}
+
+function obtenerNombreDelAmigo() {
+  return document.getElementById("amigo").value.trim();
+}
+
+function esNombreValido(nombre) {
+  return nombre !== "";
+}
+
+function limpiarCampoDeEntrada() {
+  document.querySelector("#amigo").value = "";
+}
+
+function actualizarListaVisual() {
+  const contenedorDeLista = document.querySelector("#listaAmigos");
+  contenedorDeLista.innerHTML = "";
+
+  listaDeAmigos.forEach(amigo => {
+    const elementoDeLista = document.createElement("li");
+    elementoDeLista.textContent = amigo;
+    contenedorDeLista.appendChild(elementoDeLista);
+  });
+}
+
+
+function sortearAmigoAleatorio() {
+  if (listaDeAmigos.length === 0) {
+    alert("Por favor, inserte un nombre antes de sortear");
+    return;
+  }
+
+  const indiceAleatorio = obtenerIndiceAleatorio(listaDeAmigos.length);
+  mostrarAmigoSorteado(listaDeAmigos[indiceAleatorio]);
+}
+
+function obtenerIndiceAleatorio(cantidad) {
+  return Math.floor(Math.random() * cantidad);
+}
+
+function mostrarAmigoSorteado(amigo) {
+  document.querySelector("#resultado").textContent = amigo;
+}
